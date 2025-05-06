675562a6c13f1a7a40beb5b8a3ba388f898b1d86
elexerhenry <ehytnotfakefr@gmail.com>
2025-05-06 15:54:35 -0500

    Update app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="ADHD Info App", layout="centered")
+st.title("✅ Streamlit App Works!")
+st.write("If you can see this, your deployment is successful.")
